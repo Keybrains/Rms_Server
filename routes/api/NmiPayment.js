36410cfd39fc5f2bd9890df3556af33f662103ae
sparrowcms01 <sparrowcms01@gmail.com>
2023-12-21 15:03:55 +0530

    sahil new nmi api and model for db

--- routes/api/NmiPayment.js
+++ routes/api/NmiPayment.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var NmiPayment = require("../../modals/NmiPayment");
 var PaymentPlans = require("../../modals/PaymentPlans");
+var AutoRecPayments = require("../../modals/AutoRecPayments");
 var axios = require("axios");
 var crypto = require("crypto");
 var querystring = require("querystring");
@@ -700,7 +701,137 @@ router.post("/add-customer-and-subscription", async (req, res) => {
   }
 });
 
+function webhookIsVerified(webhookBody, signingKey, nonce, sig) {
+  const calculatedSig = crypto
+    .createHmac("sha256", signingKey)
+    .update(`${nonce}.${webhookBody}`)
+    .digest("hex");
+  return sig === calculatedSig;
+}
+
 router.post("/nmi", async (req, res) => {
+  try {
+    const signingKey = "CC8775A4CFD933614985209F6F68768B";
+    const webhookBody = JSON.stringify(req.body); // Assuming body is JSON
+    const sigHeader = req.headers["webhook-signature"];
+
+    if (!sigHeader || sigHeader.length < 1) {
+      throw new Error("Invalid webhook - signature header missing");
+    }
+
+    const sigMatches = sigHeader.match(/t=(.*),s=(.*)/);
+    if (!sigMatches || sigMatches.length !== 3) {
+      throw new Error("Unrecognized webhook signature format");
+    }
+
+    const nonce = sigMatches[1];
+    const signature = sigMatches[2];
+
+    if (!webhookIsVerified(webhookBody, signingKey, nonce, signature)) {
+      throw new Error(
+        "Invalid webhook - invalid signature, cannot verify sender"
+      );
+    }
+
+    // Webhook is now verified to have been sent by you, continue processing
+    console.log("Webhook is verified");
+    const webhook = req.body; // Assuming JSON payload
+
+    if (webhook.event_type === "recurring.subscription.add") {
+      //console.log("successfully update recurring subscription");
+      // const gymOwner = await User.findOne({nmiSubscriptionId: parsedWebhook.event_body.subscription_id})
+      // if(gymOwner) {
+      const payment = await AutoRecPayments.create({
+        // gymId: gymOwner.parentId,
+        // paymentplanId: gymOwner.nmiplanId,
+        // memberId: gymOwner._id,
+        email: webhook.event_body.email,
+        description: "Recurring charging added",
+        amount: webhook.event_body.plan.amount,
+      });
+      //Save payment details of the user in payment collection
+      await payment.save();
+      //update user payment status to true
+      // await User.findOneAndUpdate(
+      //     {
+      //         nmiSubscriptionId: parsedWebhook.event_body.subscription_id,
+      //         userRole: ROLE_MEMBER
+      //     },
+      //     {
+      //         paymentStatus: true
+      //     }
+      //   );
+      // }
+    } else if (webhook.event_type === "recurring.subscription.update") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging subscription update",
+        amount: webhook.event_body.plan.amount,
+      });
+
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.auth.failure") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction auth failure",
+        amount: webhook.event_body.plan.amount,
+      });
+
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.auth.success") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction auth success",
+        amount: webhook.event_body.plan.amount,
+      });
+
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.auth.unknown") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction auth unknown",
+        amount: webhook.event_body.plan.amount,
+      });
+
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.capture.success") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction capture success",
+        amount: webhook.event_body.plan.amount,
+      });
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.credit.success") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction credit success",
+        amount: webhook.event_body.plan.amount,
+      });
+      //Save payment details of the user in payment collection
+      await payment.save();
+    } else if (webhook.event_type === "transaction.capture.failure") {
+      const payment = await AutoRecPayments.create({
+        email: webhook.event_body.email,
+        description: "Recurring charging transaction capture failure",
+        amount: webhook.event_body.plan.amount,
+      });
+      //Save payment details of the user in payment collection
+      await payment.save();
+    }
+
+    res.status(200).send("Webhook processed successfully");
+  } catch (error) {
+    console.error("Error handling webhook:", error);
+    res.status(500).send("Error processing webhook");
+  }
+});
+
+router.post("/nmis", async (req, res) => {
   try {
     console.log("yay...!!!");
     const signingKey = "CC8775A4CFD933614985209F6F68768B";
@@ -726,7 +857,7 @@ router.post("/nmi", async (req, res) => {
 
     const nonce = match[1];
     const signature = match[2];
-    console.log('SIG->',signingKey, nonce, signature);
+    console.log("SIG->", signingKey, nonce, signature);
 
     if (
       !webhookIsVerified(
@@ -743,7 +874,7 @@ router.post("/nmi", async (req, res) => {
       return;
     }
     console.log("yay2...!!!");
-    console.log('yay here is response : ', webhookBody)
+    console.log("yay here is response : ", webhookBody);
 
     // Webhook is now verified to have been sent by us, continue processing
     // const parsedWebhook = webhookBody;
@@ -785,13 +916,13 @@ router.post("/nmi", async (req, res) => {
   }
 });
 
-const webhookIsVerified = (webhookBody, signingKey, nonce, sig) => {
-  const hashedSignature = crypto
-    .createHmac("sha256", signingKey)
-    .update(nonce + "." + webhookBody)
-    .digest("hex");
-  return sig === hashedSignature;
-};
+// const webhookIsVerified = (webhookBody, signingKey, nonce, sig) => {
+//   const hashedSignature = crypto
+//     .createHmac("sha256", signingKey)
+//     .update(nonce + "." + webhookBody)
+//     .digest("hex");
+//   return sig === hashedSignature;
+// };
 
 // router.post("/custom-add-subscription", async (req, res) => {
 //   try {
