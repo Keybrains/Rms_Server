82d36942b684aa2f1247ae368253369a90f9511b
sparrowcms01 <sparrowcms01@gmail.com>
2023-12-22 20:03:44 +0530

    temp sahil

--- modals/AutoRecPayments.js
+++ modals/AutoRecPayments.js
@@ -17,6 +17,10 @@ const autoRecpaymentSchema = new Schema({
     //     ref: "users",
     //     required: true
     // },
+    nmiSubscriptionId: {
+        type:String,
+        default:null
+      },
     email: {
         type: String,
         default: ""
