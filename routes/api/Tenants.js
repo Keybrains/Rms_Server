cf5658c029e79fed25a1443879cea3d387683fb5
sparrowcms01 <sparrowcms01@gmail.com>
2023-12-26 10:01:34 +0530

    sahil temp

--- routes/api/Tenants.js
+++ routes/api/Tenants.js
@@ -1135,7 +1135,7 @@ router.post("/tenant", async (req, res) => {
         (entry) => entry.rental_adress === tenantRentalAddress
       );
 
-      console.log("Matching Entry:", matchingEntry);
+      //console.log("Matching Entry:", matchingEntry);
 
       if (
         matchingEntry &&
