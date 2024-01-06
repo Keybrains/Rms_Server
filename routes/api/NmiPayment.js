cf4b6d2b9b6012770ee3c36e6698d23aa1f84d45
Mansi <mansiandharia13@gmail.com>
2024-01-06 09:53:45 +0530

    beforepull

--- routes/api/NmiPayment.js
+++ routes/api/NmiPayment.js
@@ -765,14 +765,26 @@ router.post("/nmi", async (req, res) => {
       // console.log("email from NMI resp: ", webhook.event_body.email);
       //Save payment details of the user in payment collection
       await payment.save();
-
      // Update Tenant record with the subscription ID
-      await Tenant.findOneAndUpdate(
-        { tenant_email: webhook.event_body.billing_address.email }, // Assuming email is the matching field
-        { $set: { subscription_id: req.body.event_body.subscription_id } }
-      );
-
-     // await tenant.save();
+    //  const tenant_email = webhook.event_body.billing_address.email;
+    //  const rental = webhook.event_body.billing_address.address1;
+    //  const unit = webhook.event_body.billing_address.address2;
+    //  const subscription_id = req.body.event_body.subscription_id;
+     
+    //  const updatedTenant = await Tenant.findOneAndUpdate(
+    //    {
+    //      tenant_email: tenant_email,
+    //      'entries.rental_adress': rental,
+    //      'entries.rental_units': unit
+    //    },
+    //    {
+    //      $set: {
+    //        'entries.$.subscription_id': subscription_id
+    //      }
+    //    },
+    //    { new: true }
+    //  );
+    
       //update user payment status to true
       // await User.findOneAndUpdate(
       //     {
