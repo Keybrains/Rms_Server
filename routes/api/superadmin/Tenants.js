dcd4e89e203dfbe421a758ef371499a34dcc7893
iamshivamshukla <shivamshukla@sparrowsofttech.com>
2024-02-01 09:32:52 +0530

    shivam

--- routes/api/superadmin/Tenants.js
+++ routes/api/superadmin/Tenants.js
@@ -4,6 +4,7 @@ var Tenant = require("../../../modals/superadmin/Tenant");
 var AdminRegister = require("../../../modals/superadmin/Admin_Register");
 var Lease = require("../../../modals/superadmin/Leasing");
 var Unit = require("../../../modals/superadmin/Unit");
+var Rentals = require("../../../modals/superadmin/Rentals");
 const { createTenantToken } = require("../../../authentication");
 var moment = require("moment");
 
@@ -133,6 +134,39 @@ router.post("/tenants", async (req, res) => {
   }
 });
 
+router.put("/tenants/:tenant_id", async (req, res) => {
+  try {
+    const { tenant_id } = req.params;
+
+    // Ensure that updatedAt field is set
+    req.body.updatedAt = moment().format("YYYY-MM-DD HH:mm:ss");
+
+    const result = await Tenant.findOneAndUpdate(
+      { tenant_id: tenant_id },
+      { $set: req.body },
+      { new: true }
+    );
+
+    if (result) {
+      res.json({
+        statusCode: 200,
+        data: result,
+        message: "Tenant Updated Successfully",
+      });
+    } else {
+      res.status(202).json({
+        statusCode: 202,
+        message: "Tenant not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      statusCode: 500,
+      message: err.message,
+    });
+  }
+});
+
 router.delete("/tenant/:tenant_id", async (req, res) => {
   const tenant_id = req.params.tenant_id;
   try {
@@ -199,8 +233,6 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    
-
     const token = await createTenantToken({
       _id: tenant._id,
       tenant_id: tenant.tenant_id,
@@ -233,4 +265,47 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// ========================  Tenant ==============================================
+
+router.get("/tenant_profile/:tenant_id", async (req, res) => {
+  try {
+    const tenant_id = req.params.tenant_id;
+
+    const tenantData = await Tenant.findOne({ tenant_id: tenant_id });
+
+    if (!tenantData) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: "Tenant not found",
+      });
+    }
+
+    const data = {};
+    const leaseData = await Lease.find({ tenant_id: tenant_id });
+
+    data.tenantData = tenantData;
+    data.leaseData = [];
+    for (let i = 0; i < leaseData.length; i++) {
+      const rental_id = leaseData[i].rental_id;
+
+      const rentalData = await Rentals.findOne({ rental_id: rental_id });
+
+      data.leaseData.push({
+        lease: leaseData[i],
+        rental: rentalData,
+      });
+    }
+
+    res.json({
+      statusCode: 200,
+      data: data,
+      message: "Read All Request",
+    });
+  } catch (error) {
+    res.status(500).json({
+      statusCode: 500,
+      message: error.message,
+    });
+  }
+});
 module.exports = router;
