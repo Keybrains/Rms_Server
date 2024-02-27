7defe35bd556244f5c91a1b388244741a15790b0
sparrowcms01 <sparrowcms01@gmail.com>
2024-02-27 16:08:27 +0530

    yashpush

--- routes/api/Images.js
+++ routes/api/Images.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-const baseUrl = process.env.REACT_APP_BASE_URL;
+const baseUrl = process.env.REACT_APP_IMAGE_URL;
 
 //set currunt time to add before filename to identify
 function getCurrentDateAndTime() {
@@ -53,7 +53,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-//upload sinle and multiple files
 router.post("/upload", upload.array("files", 12), async (req, res) => {
   try {
     const uploadedFiles = req.files.map((file, index) => {
@@ -64,7 +63,7 @@ router.post("/upload", upload.array("files", 12), async (req, res) => {
         file.mimetype === "image/gif" ||
         file.mimetype === "image/bmp"
       ) {
-        const url = baseUrl + "/images/" + file.filename;
+        const url = baseUrl + "/" + file.filename;
         return {
           fileType: file.mimetype.split("/")[1],
           index: index,
@@ -72,7 +71,7 @@ router.post("/upload", upload.array("files", 12), async (req, res) => {
           url: url,
         };
       } else if (file.mimetype === "application/pdf") {
-        const url = baseUrl + "/pdf/" + file.filename;
+        const url = baseUrl + "/" + file.filename;
         return {
           fileType: file.mimetype.split("/")[1],
           index: index,
@@ -80,7 +79,7 @@ router.post("/upload", upload.array("files", 12), async (req, res) => {
           url: url,
         };
       } else {
-        const url = baseUrl + "/docs/" + file.filename;
+        const url = baseUrl + "/" + file.filename;
         return {
           fileType: file.mimetype.split("/")[1],
           index: index,
@@ -107,63 +106,37 @@ router.post("/upload", upload.array("files", 12), async (req, res) => {
   }
 });
 
-router.get("/upload/:filetype/:filename", async (req, res) => {
+router.get("/get-file/:filename", async (req, res) => {
   try {
     const filename = req.params.filename;
-    const filetype = req.params.filetype;
+    const filePath1 = path.join(
+      "../.././mern/Rms_client/files/images/",
+      filename
+    );
+    const filePath2 = path.join(
+      "../.././mern/Rms_client/files/docs/",
+      filename
+    );
+    const filePath3 = path.join("../.././mern/Rms_client/files/pdf/", filename);
 
-    // Assuming the images are stored in the 'images' directory
-
-    // Check if the file exists
-    if (filetype === "images") {
-      const filePath = path.join(
-        "../.././mern/Rms_client/files/",
-        filetype,
-        filename
+    if (fs.existsSync(filePath1)) {
+      const fileBuffer = fs.readFileSync(filePath1);
+      res.set("Content-Type", "image/jpeg");
+      res.send(fileBuffer);
+    } else if (fs.existsSync(filePath2)) {
+      const fileBuffer = fs.readFileSync(filePath2);
+      res.set(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
+          "application/msword"
       );
-      console.log(filePath);
-      if (fs.existsSync(filePath)) {
-        // Read the file and send it in the response
-        const fileBuffer = fs.readFileSync(filePath);
-        res.set("Content-Type", "image/jpeg"); // Set the correct content type (adjust based on your actual image format)
-        res.send(fileBuffer);
-      } else {
-        res.status(404).json({ status: "error", message: "Image not found" });
-      }
-    } else if (filetype === "pdf") {
-      const filePath = path.join(
-        "../.././mern/Rms_client/files/",
-        filetype,
-        filename
-      );
-      if (fs.existsSync(filePath)) {
-        // Read the file and send it in the response
-        const fileBuffer = fs.readFileSync(filePath);
-        res.set("Content-Type", "application/pdf"); // Set the correct content type (adjust based on your actual image format)
-        res.send(fileBuffer);
-      } else {
-        res.status(404).json({ status: "error", message: "Pdf not found" });
-      }
+      res.send(fileBuffer);
+    } else if (fs.existsSync(filePath3)) {
+      const fileBuffer = fs.readFileSync(filePath3);
+      res.set("Content-Type", "application/pdf");
+      res.send(fileBuffer);
     } else {
-      const filePath = path.join(
-        "../.././mern/Rms_client/files/",
-        filetype,
-        filename
-      );
-      if (fs.existsSync(filePath)) {
-        // Read the file and send it in the response
-        const fileBuffer = fs.readFileSync(filePath);
-        res.set(
-          "Content-Type",
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-            "application/msword"
-        ); // Set the correct content type (adjust based on your actual image format)
-        res.send(fileBuffer);
-      } else {
-        res
-          .status(404)
-          .json({ status: "error", message: "Document not found" });
-      }
+      res.status(404).json({ status: "error", message: "Document not found" });
     }
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
